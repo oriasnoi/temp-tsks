9eb5da922ac96e07164977d012f827826cc019d2
oriasnoi <oleksandr_riasnoi@epam.com>
2025-05-08 10:45:57 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  description = "Google Cloud Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "Region"
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "Zone"
+  default     = "us-central1-a"
+}
